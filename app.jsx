038c1303f87dd39fec80603889ea73a4540ed403
danielangeles1 <111299637+danielangeles1@users.noreply.github.com>
2024-03-24 19:33:39 -0700

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// param [address, locality(City, stat abrev, zip code)]
+// "1600 Amphitheatre Pkwy", "Mountain View, CA, 94043"
+export default function getAQI()
+{
+    window.alert("The Univeresal AQI of the specified address is: " + 17);
+}
